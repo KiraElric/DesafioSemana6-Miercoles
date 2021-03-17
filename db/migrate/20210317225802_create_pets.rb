ff8329cfe4769d423227d6284e92523c80c95c6b
KiraElric <lissette.urso@gmail.com>
2021-03-17 20:09:09 -0300

    Migracion

--- db/migrate/20210317225802_create_pets.rb
+++ db/migrate/20210317225802_create_pets.rb
@@ -1,7 +1,7 @@
 class CreatePets < ActiveRecord::Migration[5.2]
   def change
     create_table :pets do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :breed
       t.date :birthday
 
--- db/migrate/20210317230036_create_histories.rb
+++ db/migrate/20210317230036_create_histories.rb
@@ -1,10 +1,10 @@
 class CreateHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :histories do |t|
-      t.date :date
+      t.date :date, default: ->{'CURRENT_DATE'}
       t.float :weight
       t.float :height
-      t.text :description
+      t.text :description, null: false
       t.belongs_to :pet, foreign_key: true
 
       t.timestamps
