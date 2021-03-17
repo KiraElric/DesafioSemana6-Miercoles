dee7ff742cd0b2f9a3776ceaf363ea0cd5f44808
KiraElric <lissette.urso@gmail.com>
2021-03-17 20:21:43 -0300

    Modificacion archivo pets models y routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :histories
-  resources :pets
+  resources :pets do
+    resources :histories
+    #resources :histories, only: [:index, :create, :new]
+    #resources :histories, shallow: true
+  end
+
+  #resources :histories, except: [:index, :create, :new]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
