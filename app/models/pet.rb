47da80be9311bd6f8e32d8c15cc5aa9789ab0727
KiraElric <lissette.urso@gmail.com>
2021-03-17 21:47:21 -0300

    Parte 1 terminada del desafio

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,25 @@ class Pet < ApplicationRecord
   def to_s #Devolver el nombre de la mascota
     return name
   end
+
+  def histories_count #Metodo para contar la cantidad de veces que ha visitado al vet
+    self.histories.count
+  end
+
+  def average_weight #Metodo del promedio de los pesos de la mascota
+    # self.histories.pluck(:weight).sum / self.histories_count Otra forma de hacer lo mismo de abajo
+    self.histories.sum(:weight) / self.histories_count 
+  end
+
+  def average_height #Metodo del promedio de las alturas de la mascota
+    self.histories.sum(:height) / self.histories_count
+  end
+
+  def max_weight
+    self.histories.maximum(:weight)
+  end
+
+  def max_height
+    self.histories.maximum(:height)
+  end
 end
